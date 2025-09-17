88d9a16b36493daab0fcd7cd4d4415f609c02643
Steaf23 <steafmobile@gmail.com>
2025-09-17 02:08:33 +0200

    feat(godot): updated to 4.5
    feat(enemy): added enemy animations

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -6,18 +6,24 @@ extends CharacterBody2D
 @onready var ai_controller: AIController = $AIController
 @onready var attack_cooldown: Timer = %AttackCooldown
 
+var attacking: bool = false
 
-func _physics_process(delta: float) -> void:
-	velocity = ai_controller.desired_velocity
-	move_and_slide()
+func _physics_process(_delta: float) -> void:
+	if not attacking:
+		velocity = ai_controller.desired_velocity
+		move_and_slide()
 
 
 func _on_hit_box_body_entered(body: Node2D) -> void:
 	if body is Turret and attack_cooldown.is_stopped():
 		attack_cooldown.start()
 		body.damage(damage)
+		$Sprite2D.play("swing")
 		$HitBox/CollisionShape2D.set_deferred("disabled", true)
-
+		attacking = true
+		await get_tree().create_timer($%AttackCooldown.wait_time / 2).timeout
+		attacking = false
+		$Sprite2D.play("walk")
 
 func _on_attack_cooldown_timeout() -> void:
 	$HitBox/CollisionShape2D.disabled = false
@@ -25,7 +31,7 @@ func _on_attack_cooldown_timeout() -> void:
 		_on_hit_box_body_entered(b)
 
 
-func take_damage(amount: int) -> void:
+func take_damage(_amount: int) -> void:
 	$HPBar.current_hp -= 1
 	
 	if $HPBar.current_hp <= 0:
